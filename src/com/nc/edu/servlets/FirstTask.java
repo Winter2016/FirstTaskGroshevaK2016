7b29de6b04d68f584859e0be576aa67a0671ad94
Winter2016 <tsmgofm@mail.ru>
2016-02-28 16:15:49 +0300

    Первое задание с методом GET.

---
+++ src/com/nc/edu/servlets/FirstTask.java
@@ -0,0 +1,28 @@
+package com.nc.edu.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Ксения on 2/27/2016.
+ */
+public class FirstTask extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse reresp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String password = req.getParameter("password");
+        PrintWriter pw = reresp.getWriter();
+        pw.write("<b> Hello " + name + "</b>");
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String password = req.getParameter("password");
+        PrintWriter pw = resp.getWriter();
+        pw.write("<b> Hello " + name + "</b>");
+    }
+}
